db9f27fc83c9c40dde12635181940fae1cdeb6c5
unknown <diloch111@gmail.com>
2023-09-02 14:55:01 +0100

    create the hero div and image element and append to parents

--- assets/script.js
+++ assets/script.js
@@ -18,99 +18,90 @@ let favoriteAlbum = {
 };
 
 // dynamically create the variables, add classes and append them
-// div with class of container
+    // div with class of container
 const body = document.body; // create the body variable
 const containerDiv = document.createElement("div"); // create the main div
 containerDiv.setAttribute("class", "container"); // added container class to the main div
 body.append(containerDiv); // append the main div element to the body
-
-// div with class of artist for the main heading
+    // div with class of hero for the image
+const heroDiv = document.createElement("div"); // create a div to wrap the hero image
+containerDiv.append(heroDiv); // append the hero div to the main container div
+const imgEl = document.createElement("img"); // create the hero element image
+heroDiv.append(imgEl); // append the new image element to the hero div
+    // div with class of artist for the main heading
 const artistDiv = document.createElement("div"); // create the the div for the artist heading
 artistDiv.setAttribute("class", "artist"); // add a class of artist for the artistDiv
 containerDiv.append(artistDiv); // append the artist heading to the main container
-
-// div with class of album for the album heading
+    // div with class of album for the album heading
 const albumDiv = document.createElement("div"); // create a div for the album heading
 albumDiv.setAttribute("class", "album"); // add a class to album to the album heading div
 containerDiv.append(albumDiv); // append the album div to the main container div
-
-// div with class of track for the album tracks title
+    // div with class of track for the album tracks title
 const tracksTitleDiv = document.createElement("div"); // create div to hold the tracks title
 tracksTitleDiv.setAttribute("class", "track-title"); // add class of track-title to the track title div
 containerDiv.append(tracksTitleDiv); // append the track title div to the main container div
-
-// div with class of track-list for the album tracks
+    // div with class of track-list for the album tracks
 const trackListDiv = document.createElement("div"); // create div for track list
 trackListDiv.setAttribute("class", "track-list"); // class of track-list for the tracks list
 tracksTitleDiv.append(trackListDiv); // append the track list to track list title
 
 // dynamically add content to the various sections
-// main header div
+    // main header div
 const headingOne = document.createElement("h1"); // create h1 element
 artistDiv.append(headingOne); // append the h1 element to the artist div
 headingOne.innerText = favoriteAlbum.Artist; // set the artist content from the object to the h1 element
-
-// artist div
+    // artist div
 const headingTwo = document.createElement("h2"); // create h2 element
 albumDiv.append(headingTwo); // append the h2 to the album div
 headingTwo.innerText = favoriteAlbum.Album; // set the album content from the object to the h2 element
-
-// tracks
+    // tracks
 const ulEl = document.createElement("ul"); // create unordered list element
 trackListDiv.append(ulEl); // append the unordered list to the tracks list div
 
 // TO-DO: a function could actually do this faster (grab the tracks from the object and add them by innerText )
-//track one
+    //track one
 const trackOne = document.createElement("li"); // create the track 1 list element
 ulEl.append(trackOne); // append the track one list element to the unordered list
 trackOne.innerText = favoriteAlbum.Tracks[0]; // set the track one list item to the track one content inside the object
-
-//track two
+    //track two
 const trackTwo = document.createElement("li"); // create the track 1 list element
 ulEl.append(trackTwo); // append the track one list element to the unordered list
 trackTwo.innerText = favoriteAlbum.Tracks[1]; // set the track one list item to the track one content inside the object
-
-//track Three
+    //track Three
 const trackThree = document.createElement("li"); // create the track 1 list element
 ulEl.append(trackThree); // append the track one list element to the unordered list
 trackThree.innerText = favoriteAlbum.Tracks[2]; // set the track one list item to the track one content inside the object
-
-//track Four
+    //track Four
 const trackFour = document.createElement("li"); // create the track 1 list element
 ulEl.append(trackFour); // append the track one list element to the unordered list
 trackFour.innerText = favoriteAlbum.Tracks[3]; // set the track one list item to the track one content inside the object
-
-//track Five
+    //track Five
 const trackFive = document.createElement("li"); // create the track 1 list element
 ulEl.append(trackFive); // append the track one list element to the unordered list
 trackFive.innerText = favoriteAlbum.Tracks[4]; // set the track one list item to the track one content inside the object
-
-//track Six
+    //track Six
 const trackSix = document.createElement("li"); // create the track 1 list element
 ulEl.append(trackSix); // append the track one list element to the unordered list
 trackSix.innerText = favoriteAlbum.Tracks[5]; // set the track one list item to the track one content inside the object
-
-//track Seven
+    //track Seven
 const trackSeven = document.createElement("li"); // create the track 1 list element
 ulEl.append(trackSeven); // append the track one list element to the unordered list
 trackSeven.innerText = favoriteAlbum.Tracks[6]; // set the track one list item to the track one content inside the object
-
-//track Eight
+    //track Eight
 const trackEight = document.createElement("li"); // create the track 1 list element
 ulEl.append(trackEight); // append the track one list element to the unordered list
 trackEight.innerText = favoriteAlbum.Tracks[7]; // set the track one list item to the track one content inside the object
-
-//track Nine
+    //track Nine
 const trackNine = document.createElement("li"); // create the track 1 list element
 ulEl.append(trackNine); // append the track one list element to the unordered list
 trackNine.innerText = favoriteAlbum.Tracks[8]; // set the track one list item to the track one content inside the object
-
-//track Ten
+    //track Ten
 const trackTen = document.createElement("li"); // create the track 1 list element
 ulEl.append(trackTen); // append the track one list element to the unordered list
 trackTen.innerText = favoriteAlbum.Tracks[9]; // set the track one list item to the track one content inside the object
-
-//track Eleven
+    //track Eleven
 const trackEleven = document.createElement("li"); // create the track 1 list element
 ulEl.append(trackEleven); // append the track one list element to the unordered list
 trackEleven.innerText = favoriteAlbum.Tracks[10]; // set the track one list item to the track one content inside the object
+
+// Time to style
