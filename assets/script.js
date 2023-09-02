e0d2aaf382bb2be20b711b516531327f897f750d
unknown <diloch111@gmail.com>
2023-09-02 12:32:23 +0100

    add the album object

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// create the album object
+let favoriteAlbum = {
+  Artist: "Marcus Cole",
+  Album: "Write my Song",
+  Tracks: [
+    "Track 1: That's Alright (Cause I gat You)",
+    "Track 2: The Lord Has Been Good",
+    "Track 3: Finally",
+    "Track 4: Remember to Breath",
+    "Track 5: I Don't Know",
+    "Track 6: Write My Song",
+    "Track 7: Being with You",
+    "Track 8: Persuaded",
+    "Track 9: Celebrate",
+    "Track 10: You're There",
+    "Track 11: Bless My Soul"
+  ],
+};
