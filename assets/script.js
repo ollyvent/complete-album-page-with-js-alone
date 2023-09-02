30af23b2bd0a6864733ebd3efa719cfcc1639fb4
unknown <diloch111@gmail.com>
2023-09-02 13:17:18 +0100

    add div for the album element and append to main container div

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,13 @@ let favoriteAlbum = {
 };
 
 // dynamically create the variables, add classes and append them
+    // div with class of container
 const body = document.body; // create the body variable
 const containerDiv = document.createElement("div"); // create the main div
 containerDiv.setAttribute("class", "container"); // added container class to the main div
 body.append(containerDiv); // append the main div element to the body
+    // div with class of artist for the main heading
+const artistDiv = document.createElement("div"); // create the the div for the artist heading
+artistDiv.setAttribute("class", "artist");  // add a class of artist for the artistDiv
+containerDiv.append(artistDiv); // append the artist heading to the main container
+    
