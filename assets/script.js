25c22f66ded4bb2e1509aa0d708a61556c0651d6
unknown <diloch111@gmail.com>
2023-09-02 13:41:34 +0100

    create track list div and append to track list title

--- assets/script.js
+++ assets/script.js
@@ -18,21 +18,24 @@ let favoriteAlbum = {
 };
 
 // dynamically create the variables, add classes and append them
-    // div with class of container
+// div with class of container
 const body = document.body; // create the body variable
 const containerDiv = document.createElement("div"); // create the main div
 containerDiv.setAttribute("class", "container"); // added container class to the main div
 body.append(containerDiv); // append the main div element to the body
-    // div with class of artist for the main heading
+// div with class of artist for the main heading
 const artistDiv = document.createElement("div"); // create the the div for the artist heading
-artistDiv.setAttribute("class", "artist");  // add a class of artist for the artistDiv
+artistDiv.setAttribute("class", "artist"); // add a class of artist for the artistDiv
 containerDiv.append(artistDiv); // append the artist heading to the main container
-    // div with class of album for the album heading
+// div with class of album for the album heading
 const albumDiv = document.createElement("div"); // create a div for the album heading
 albumDiv.setAttribute("class", "album"); // add a class to album to the album heading div
-containerDiv.append(albumDiv);  // append the album div to the main container div
-    // div with class of track for the album tracks title
+containerDiv.append(albumDiv); // append the album div to the main container div
+// div with class of track for the album tracks title
 const tracksTitleDiv = document.createElement("div"); // create div to hold the tracks title
 tracksTitleDiv.setAttribute("class", "track-title"); // add class of track-title to the track title div
 containerDiv.append(tracksTitleDiv); // append the track title div to the main container div
-    
+// div with class of track-list for the album tracks
+const trackListDiv = document.createElement("div"); // create div for track list
+trackListDiv.setAttribute("class", "track-list"); // class of track-list for the tracks list
+tracksTitleDiv.append(trackListDiv); // append the track list to track list title
